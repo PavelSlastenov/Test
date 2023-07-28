13e99b910e7bad78cecaea403db6fb71f0bd4d0c
“Slastenov <vtb4055018@vtb.ru>
2023-07-28 08:49:03 +0300

    added OrderPageFirefoxTest

--- src/test/java/OrderPageFireFoxTest.java
+++ src/test/java/OrderPageFireFoxTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import page_object.MainPage;
 import page_object.OrderPage;
